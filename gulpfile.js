4c8cfb60e90d7a11da1c1b50bb6858763279bed3
John W Custer <johnwcuster@users.noreply.github.com>
2019-08-08 11:20:00 -0400

    Version 1 -- layout & menu interactions

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,161 @@
-"use strict";
-
-var gulp = require("gulp"),
-  // autoprefixer = require("autoprefixer"),
+// initialize modules
+const {src, dest, watch, series, parallel} = require("gulp"),
+  autoprefixer = require("autoprefixer"),
   // cleanCSS = require("gulp-clean-css"),
-  // cssnano = require("gulp-cssnano"),
-  del = require("del"),
-  eslint = require("gulp-eslint"),
+  cssnano = require("cssnano"),
+  concat = require("gulp-concat"),
+//  eslint = require("gulp-eslint"),
   postcss = require("gulp-postcss"),
+  replace = require("gulp-replace"),
+  sass = require("gulp-sass"),
+  // scss_lint = require("gulp-stylelint"),
   sourcemaps = require("gulp-sourcemaps"),
-  sass = require("gulp-sass");
+  uglify = require("gulp-uglify");
+
+// file path variables
+const files = {
+  scssPath: "./src/scss/**/*.scss",
+  jsPath: "./src/js/**/*.js"
+}
+
+// sass task
+function scssTask(){
+    return src(files.scssPath)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        // .pipe(scss_lint({
+        //   reporters: [
+        //     {formatter: 'string', console: true}
+        //   ]
+        // }))
+        .pipe(postcss([ autoprefixer(), cssnano() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist')
+    );
+}
+
+// JS Task
+function jsTask(){
+    return src([files.jsPath])
+        .pipe(concat('all.js'))
+        // .pipe(uglify())
+        .pipe(dest('dist')
+    );
+}
+
+// Cache busting task
+var cbString = new Date().getTime();
+function cacheBustTask(){
+    return src(['index.html'])
+        .pipe(replace(/\?cb=\d+/g, '?cb=' + cbString))
+        .pipe(dest('.'));
+}
 
 
-gulp.task("sass", function () {
-  return gulp.src("./src/scss/**/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass.sync().on("error", sass.logError))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./src/css"));
-});
+// Watch Task
+function watchTask(){
+    watch(
+        [files.scssPath, files.jsPath],
+        parallel(scssTask, jsTask)
+    );
+}
+
+// Default Task
+exports.default = series(
+  parallel(scssTask, jsTask),
+  cacheBustTask,
+  watchTask
+);
 
 
-gulp.task("watchSass", function () {
-  gulp.watch("./src/scss/**/*.scss", ["sass"]);
-});
-
-
-gulp.task("postcss", ["sass"], function () {
-  return gulp.src("./src/css/**/*.css")
-    .pipe( sourcemaps.init() )
-    .pipe( postcss([ require("autoprefixer"), require("cssnano") ]) )
-    .pipe( sourcemaps.write(".") )
-    .pipe( gulp.dest("../aiddata4/css/dist") );
-});
-
-gulp.task("eslint", function(){
-  return gulp.src("./src/js/**/*.js")
-    .pipe(eslint({ fix: true }))
-    .pipe(eslint.format())
-    .pipe(gulp.dest("./src/js/."));
-});
-
-gulp.task("writeJS", ["eslint"], function(){
-  return gulp.src("./src/js/**/*.js")
-    .pipe(gulp.dest("./src/js/**/*.js"))
-});
-
-gulp.task("clean", function() {
-  del(["dist", "./src/css/*"]);
-  del(["dist", "./*.css"]);
-  del(["dist", "./*.css.map"]);
-});
-
-gulp.task("default", ["clean"], function() {
-  gulp.start("sass");
-  gulp.start("postcss");
-  gulp.start("writeJS");
-});
-
-
-
-
+// "use strict";
+//
+// const gulp = require("gulp"),
+//   // autoprefixer = require("autoprefixer"),
+//   // cleanCSS = require("gulp-clean-css"),
+//   // cssnano = require("gulp-cssnano"),
+//   del = require("del"),
+//   eslint = require("gulp-eslint"),
+//   postcss = require("gulp-postcss"),
+//   sourcemaps = require("gulp-sourcemaps"),
+//   sass = require("gulp-sass");
+//
+//
+// gulp.task("sass", function (done) {
+//   return gulp.src("./src/scss/**/*.scss")
+//     .pipe(sourcemaps.init())
+//     .pipe(sass.sync().on("error", sass.logError))
+//     .pipe(sourcemaps.write("."))
+//     .pipe(gulp.dest("./src/css"));
+//     done();
+// });
+//
+//
+// gulp.task("watchSass", function (done) {
+//   gulp.watch("./src/scss/**/*.scss", ["sass"]);
+//   done();
+// });
+//
+//
+// // problem
+// // gulp.task("postcss", ["sass"], function () {
+// //   return gulp.src("./src/css/**/*.css")
+// //     .pipe( sourcemaps.init() )
+// //     .pipe( postcss([ require("autoprefixer"), require("cssnano") ]) )
+// //     .pipe( sourcemaps.write(".") )
+// //     .pipe( gulp.dest("./src/css") );
+// // });
+//
+// gulp.task("postcss", gulp.series("sass", function(done){
+//   return gulp.src("./src/css/**/*.css")
+//     .pipe( sourcemaps.init() )
+//     .pipe( postcss([ require("autoprefixer"), require("cssnano") ]) )
+//     .pipe( sourcemaps.write(".") )
+//     .pipe( gulp.dest("./src/css") );
+//     done();
+// }));
+//
+//
+// gulp.task("eslint", function(done){
+//   return gulp.src("./src/js/**/*.js")
+//     .pipe(eslint({ fix: true }))
+//     .pipe(eslint.format())
+//     .pipe(gulp.dest("./src/js/."));
+//     done();
+// });
+//
+// // problem
+// // gulp.task("writeJS", ["eslint"], function(){
+// //   return gulp.src("./src/js/**/*.js")
+// //     .pipe(gulp.dest("./src/js/**/*.js"))
+// // });
+//
+// gulp.task("writeJS", gulp.series("eslint", function(done){
+//   return gulp.src("./src/js/**/*.js")
+//     .pipe(gulp.dest("./src/js/**/*.js"))
+//     done();
+// }));
+//
+//
+// gulp.task("clean", function(done) {
+//   del(["dist", "./src/css/*"]);
+//   del(["dist", "./*.css"]);
+//   del(["dist", "./*.css.map"]);
+//   done();
+// });
+//
+// // problem
+// //
+// // gulp.task("default", ["clean"], function() {
+// //   gulp.start("sass");
+// //   gulp.start("postcss");
+// //   gulp.start("writeJS");
+// // });
+// //
+// gulp.task("default", gulp.series("clean", function(done){
+//   gulp.parallel(gulp.series("sass", "postcss"), "writeJS");
+//   // gulp.start("sass");
+//   // gulp.start("postcss");
+//   // gulp.start("writeJS");
+//   done();
+// }));
---
+++ src/js/toggle_menus.js
@@ -0,0 +1,152 @@
+(function(){
+
+// config: also see:  ../src/scss/_config.scss for breakpoints
+const $sm_bp = 576,
+  $md_bp = 768,
+  $l_bp =  992,
+  $xl_bp = 1200;
+
+
+
+const docBody = document.body;
+  siteTitle = document.getElementById("site_title"),
+  controlsToggle = document.getElementById("toggle_control_panel"),
+  controlsClose = document.getElementById("close_control_panel"),
+  controlsCloseOverlay = document.getElementById("control_panel_overlay"),
+  controlPanel = document.getElementById("control_panel"),
+  viewsToggle = document.getElementById("toggle_view_select"),
+  viewsClose = document.getElementById("close_view_select"),
+  viewsPanel = document.getElementById("select_view"),
+  typesToggle = document.getElementById("toggle_type_select"),
+  typesClose = document.getElementById("close_type_select"),
+  typesPanel = document.getElementById("select_types"),
+  countriesToggle = document.getElementById("toggle_country_select"),
+  countriesClose = document.getElementById("close_country_select"),
+  countriesPanel = document.getElementById("select_countries"),
+  chartHeader = document.getElementById("chart_header"),
+  chartTitle = document.getElementById("chart_title")
+  chartSubTitle = document.getElementById("chart_subtitle"),
+  chartLabel = document.getElementById("chart_label"),
+  siteFooterLogos = document.getElementById("site_footer_logos"),
+  financeListItem = document.getElementById("finance_list_item"),
+  financeTypeOption = document.getElementById("21_diplomacy_type"),
+  financeSubTypeList = document.getElementById("finance_subtypes"),
+  pdTypeSelect = document.getElementsByName("pd_type");
+
+  for(let i=0; i<pdTypeSelect.length; i++) {
+    pdTypeSelect[i].addEventListener("input", showFinanceSubTypes, true);
+  }
+
+
+docBody.addEventListener("keydown", addKeyboardFocus, true);
+controlsToggle.addEventListener("click", toggleControlPanel, true);
+controlsCloseOverlay.addEventListener("click", closeControlPanel, true);
+controlsClose.addEventListener("click", toggleControlPanel, true);
+viewsToggle.addEventListener("click", toggleViewsPanel, true);
+viewsClose.addEventListener("click", toggleViewsPanel, true);
+typesToggle.addEventListener("click", toggleTypesPanel, true);
+typesClose.addEventListener("click", toggleTypesPanel, true);
+countriesToggle.addEventListener("click", toggleCountriesPanel, true);
+countriesClose.addEventListener("click", toggleCountriesPanel, true);
+
+financeTypeOption.addEventListener("input", showFinanceSubTypes, true);
+
+
+function getWidth() {
+  let width = document.body.clientWidth;
+  return width;
+}
+
+function addKeyboardFocus(e) {
+
+  let links = document.getElementsByTagName("a"),
+    buttons = document.getElementsByTagName("button"),
+    inputs = document.getElementsByTagName("input"),
+    labels = document.getElementsByTagName("label");
+
+    for(let a=0; a<links.length; a++) {
+      let link = links[a];
+      link.classList.add("show_keyboard_focus");
+    }
+    for(let b=0; b<buttons.length; b++) {
+      let button = buttons[b];
+      button.classList.add("show_keyboard_focus");
+    }
+    for(let i=0; i<inputs.length; i++) {
+      let input = inputs[i];
+      input.classList.add("show_keyboard_focus");
+    }
+    for(let l=0; l<labels.length; l++) {
+      let label = labels[l];
+      label.classList.add("show_keyboard_focus");
+    }
+
+
+}
+
+function closeControlPanel() {
+
+    controlPanel.classList.remove("js_visible");
+    controlsCloseOverlay.classList.remove("js_visible");
+
+    if(document.body.clientWidth >= $l_bp) {
+      controlsToggle.classList.remove("is_animated");
+      siteTitle.classList.remove("is_animated");
+      chartHeader.classList.remove("is_animated");
+      siteFooterLogos.classList.remove("is_animated");
+    }
+
+}
+
+function toggleControlPanel(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  controlPanel.classList.toggle("js_visible");
+  if(document.body.clientWidth < $l_bp) {
+    controlsCloseOverlay.classList.toggle("js_visible");
+  }
+  if(document.body.clientWidth >= $l_bp) {
+    controlsToggle.classList.toggle("is_animated");
+    siteTitle.classList.toggle("is_animated");
+    chartHeader.classList.toggle("is_animated");
+    siteFooterLogos.classList.toggle("is_animated");
+  }
+}
+
+function toggleViewsPanel(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  viewsPanel.classList.toggle("js_visible");
+  closeControlPanel();
+}
+
+function toggleTypesPanel(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  typesPanel.classList.toggle("js_visible");
+  closeControlPanel();
+
+}
+
+function toggleCountriesPanel(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  countriesPanel.classList.toggle("js_visible");
+  closeControlPanel();
+}
+
+function showFinanceSubTypes(e) {
+  let elem = e.target;
+
+  if (elem.value === "21") {
+    financeTypeOption.classList.add("js_toggled");
+    financeListItem.classList.add("js_toggled");
+    financeSubTypeList.classList.add("js_toggled");
+  } else {
+    financeTypeOption.classList.remove("js_toggled");
+    financeListItem.classList.remove("js_toggled");
+    financeSubTypeList.classList.remove("js_toggled");
+  }
+}
+
+}());
